b8b09a7060182336972dfbc9f1df0f4d1b5b404d
jamn0110 <jamn011020@gmail.com>
2025-04-24 19:31:37 -0500

    Specializations were added

--- src/main/java/edu/project/medicalofficemanagement/enums/specialization/Specialization.java
+++ src/main/java/edu/project/medicalofficemanagement/enums/specialization/Specialization.java
@@ -1,4 +1,13 @@
 package edu.project.medicalofficemanagement.enums.specialization;
 
 public enum Specialization {
+    CARDIOLOGY,
+    PEDIATRICS,
+    NEUROLOGY,
+    DERMATOLOGY,
+    ORTHOPEDICS,
+    ONCOLOGY,
+    RADIOLOGY,
+    ANESTHESIOLOGY
+
 }
